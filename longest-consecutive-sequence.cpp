3a370a9f266fb552eb86cf6c520fa7a36c40222e
Dhruvam1111 <panchaldhruvam19@gmail.com>
2025-04-03 17:06:22 +0530

    Updated

--- longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence.cpp
@@ -6,23 +6,27 @@ class Solution
 public:
     int longestConsecutive(vector<int> &nums)
     {
-        unordered_set<int> h;
+        unordered_map<int, bool> h;
         int longest = 0;
-        for (int i = 0; i < nums.size(); i++)
+        for (int num : nums)
         {
-            h.insert(nums[i]);
+            h[num] = true;
         }
-        for (auto i : h)
+
+        for (auto pair : h)
         {
+            int i = pair.first;
             if (h.find(i - 1) == h.end())
             {
                 int count = 1;
                 int element = i;
+
                 while (h.find(element + 1) != h.end())
                 {
                     count++;
                     element++;
                 }
+
                 longest = max(longest, count);
             }
         }
